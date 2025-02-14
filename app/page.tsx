99f4cb449fe66f2eb97df2b91458e197544ca512
LiponRoy <liponbdbd@gmail.com>
2025-02-14 14:35:09 +0600

    installed shadcn ui and configured

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+
 export default function Home() {
   return (
     <div className=" h-screen w-full flex flex-col justify-center items-center gap-y-4">
+
       <span className=" text-2xl">Nest Employee</span>
       <span>Project Start </span>
 
