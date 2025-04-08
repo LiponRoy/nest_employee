12303a4f12471fe22b12e3d0ef735b50a151ae97
lipon <liponbdbd@gmail.com>
2025-04-08 12:21:45 +0600

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,14 +16,11 @@ import { useEffect, useState } from "react";
 //
 import {
     Avatar,
-    AvatarFallback,
-    AvatarImage,
 } from "@/components/ui/avatar"
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
-    DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
