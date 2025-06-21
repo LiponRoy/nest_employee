8a0e915e131325d74b9b87ea3c549229d4ef8319
LiponRoy <liponbdbd@gmail.com>
2025-06-22 00:33:46 +0600

    redesign posted jobs page

--- app/dashboard/getCompanyByCreator/page.tsx
+++ app/dashboard/getCompanyByCreator/page.tsx
@@ -6,11 +6,24 @@ const page = () => {
     const { data: company, error, isLoading } = useGetCompanyByCreatorQuery();
 
     return (
-        <div>
-            {company?.data?.map((val: any, i: any) => (
-                <div key={i} className="grid grid-cols-4 m-2 border border-slate-200 gap-2">
-                    <span>{val.name}</span>
+        // <div>
+        //     {company?.data?.map((val: any, i: any) => (
+        //         <div key={i} className="grid grid-cols-4 m-2 border border-slate-200 gap-2">
+        //             <span>{val.name}</span>
 
+        //         </div>
+        //     ))}
+        // </div>
+                <div className="w-full">
+            {company?.data?.map((val: any, i: any) => (
+                <div
+                    key={i}
+                    className="grid grid-cols-4 m-4 border border-slate-200 "
+                >
+                    <div className="w-full flex flex-col justify-center items-start m-2">
+                        <span>{val.name}</span>
+                        <span className="w-full bg-slate-200 px-1 rounded-md">{val.location}</span>
+                    </div>
                 </div>
             ))}
         </div>
--- app/dashboard/getJobByCreator/page.tsx
+++ app/dashboard/getJobByCreator/page.tsx
@@ -1,20 +1,25 @@
-"use client"
-import { useGetJobByCreatorQuery } from '@/redux/rtk/jobsApi';
-import React from 'react'
+"use client";
+import { useGetJobByCreatorQuery } from "@/redux/rtk/jobsApi";
+import React from "react";
 
 const page = () => {
     const { data: job, error, isLoading } = useGetJobByCreatorQuery();
 
     return (
-        <div>
+        <div className="w-full">
             {job?.data?.map((val: any, i: any) => (
-                <div key={i} className="flex flex-col justify-center items-center m-2 border border-slate-200">
-                    <span>{val.title}</span>
-
+                <div
+                    key={i}
+                    className="grid grid-cols-4 m-4 border border-slate-200 "
+                >
+                    <div className="flex flex-col justify-center items-start m-2">
+                        <span>{val.title}</span>
+                        <span className="bg-slate-200 px-1 rounded-md">{val.jobType}</span>
+                    </div>
                 </div>
             ))}
         </div>
-    )
-}
+    );
+};
 
-export default page
+export default page;
